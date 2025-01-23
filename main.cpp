824dcf6ab7b2e9dfe6db97a59f7bcd8233ee237c
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-22 20:58:43 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@ int main(){
 	array < string, numBooks > bookTitle {"Star Wars", "The Empire Strikes Back (Star Wars)",
 						"Splinter of the Mind's Eye (Star Wars)", "Return of the Jedi (Star Wars)",
 						"Heir to the Empire (Star Wars: The Thrawn Trilogy, Vol. 1)", 
-						"Dark Force Rising (Star Wars: The Thrawn Trilogy, Vol. 2",
+						"Dark Force Rising (Star Wars: The Thrawn Trilogy, Vol. 2)",
 						"The Last Command (Star Wars: The Thrawn Trilogy)",
 						"Specter of the Past (Star Wars: The Hand of Thrawn #1)",
 						"Vision of the Future (Star Wars: The Hand of Thrawn, Book 2)",
 						"Battlestar Galactica", "New Battlestar Galactica Volume 1 (Battlestar Galactica (Dynamite)) (v. 1)",
-						"New Battlestar Galactica Volume II Hardcover (Battlestar Galactica (Dynamite)","The Hobbit",
+						"New Battlestar Galactica Volume II Hardcover (Battlestar Galactica (Dynamite))","The Hobbit",
 						"The Lord of the Rings: 50th Anniversary, One Vol. Edition", "The Two Towers", "The Return of the King",
 						"The Fellowship of the Ring", "C++ Programming: From Problem Analysis to Program Design 7th Edition",
 						"Digital Computer Electronics", "Assembly Language for x86 Processors (7th Edition)"};
@@ -33,7 +33,9 @@ int main(){
 	cin >> choice;
 
 	cout << "Enter the book title you would like to seaarch for\n";
-	cin >> toSearch;
+	cin.ignore();
+	getline(cin, toSearch);
+
 
 		// Pass the string to the function to look up for it
 		result = SearchString ( bookTitle, toSearch );
@@ -44,7 +46,7 @@ int main(){
 			cout << bookTitle[result] << endl;
 		}
 			
-		else if 
+		else
 		{
 			cout << "The string " << toSearch << " is NOT found ..\n";
 		}
@@ -61,10 +63,8 @@ int index = 0;
 string *find;
 while ( index < numBooks )
 {
-	find = strstr ( titles[index] , userInput);
-
 	// If the substring is found
-	if (find)
+	if (titles[index].find (userInput) != string::npos )
 		return index;
 }
 
